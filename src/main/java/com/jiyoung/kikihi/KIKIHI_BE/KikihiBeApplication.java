63bffd69c397e8e1e3c5b32f01885ffb63f762a0
hwangjiyoung02 <hjy020714@gmail.com>
2025-03-19 17:19:18 +0900

    🔨chore : 초기 환경 세팅

---
+++ src/main/java/com/jiyoung/kikihi/KIKIHI_BE/KikihiBeApplication.java
@@ -0,0 +1,13 @@
+package com.jiyoung.kikihi.KIKIHI_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KikihiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KikihiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiyoung/kikihi/KIKIHI_BE/KikihiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiyoung.kikihi.KIKIHI_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KikihiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
