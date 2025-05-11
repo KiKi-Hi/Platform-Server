1cf44d21d04454d6816516f9482677b91030755a
eedo_y <doup2001@naver.com>
2025-05-11 23:15:34 +0900

    🔨chore/KIKI-28 : Config 설정

--- src/main/java/com/jiyoung/kikihi/KikihiBeApplication.java
+++ src/main/java/com/jiyoung/kikihi/KikihiBeApplication.java
@@ -1,9 +1,11 @@
-package com.jiyoung.kikihi.KIKIHI_BE;
+package com.jiyoung.kikihi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // Auditing 활성화
 public class KikihiBeApplication {
 
 	public static void main(String[] args) {
