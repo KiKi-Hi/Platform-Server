b900cf70719a52cc302db37d9f393dc873fd3f66
hwangjiyoung02 <hjy020714@gmail.com>
2025-05-16 01:59:06 +0900

    ✨ feat: User관련 포트 생성

---
+++ src/main/java/com/jiyoung/kikihi/platform/application/in/auth/AuthUseCase.java
@@ -0,0 +1,8 @@
+package com.jiyoung.kikihi.platform.application.in.auth;
+
+import com.jiyoung.kikihi.global.auth.oauth2.kakao.KaKaoDto;
+import com.jiyoung.kikihi.platform.domain.user.User;
+
+public interface AuthUseCase {
+    User joinUser(KaKaoDto.KakaoUserInfoResponse userInfo);
+}
