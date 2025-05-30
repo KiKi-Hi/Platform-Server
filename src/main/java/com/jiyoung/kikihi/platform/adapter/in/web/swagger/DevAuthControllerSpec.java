7fedf8529d81e3d77a94ee6d0f65079d73ae8e0c
eedo_y <doup2001@naver.com>
2025-05-30 22:16:49 +0900

    ️️✨ feat/KIKI-31 : JWT 개발용 컨트롤러 추가 및 스웨거 구현

---
+++ src/main/java/com/jiyoung/kikihi/platform/adapter/in/web/swagger/DevAuthControllerSpec.java
@@ -0,0 +1,15 @@
+package com.jiyoung.kikihi.platform.adapter.in.web.swagger;
+
+import com.jiyoung.kikihi.global.response.ApiResponse;
+import com.jiyoung.kikihi.security.jwt.dto.JWTTokenDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "개발용 JWT 발급 API", description = "개발용 JWT 발급 API입니다.")
+public interface DevAuthControllerSpec {
+
+    @Operation(
+            summary = "개발의 편의를 위해 개발용 JWT를 즉시 발급 받을 수 있습니다."
+    )
+    ApiResponse<JWTTokenDto> devLogin();
+}
