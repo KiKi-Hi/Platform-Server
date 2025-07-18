8de449a801313b15c02750af7347af8ed8e031db
eedo_y <doup2001@naver.com>
2025-07-18 14:56:46 +0900

    🔥fix/KIKI-CORS : CORS 문제 수정

--- src/main/java/com/jiyoung/kikihi/global/config/WebConfig.java
+++ src/main/java/com/jiyoung/kikihi/global/config/WebConfig.java
@@ -1,14 +1,11 @@
 package com.jiyoung.kikihi.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableSpringDataWebSupport
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${spring.front.host}")
@@ -20,16 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${spring.back.prod}")
     private String backProd;
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(localHost, frontProd, backProd)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(localHost, frontProd, backProd)
-                        .allowedMethods("*")
-                        .allowCredentials(true);
-            }
-        };
-    }}
