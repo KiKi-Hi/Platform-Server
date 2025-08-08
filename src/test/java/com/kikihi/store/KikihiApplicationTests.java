c884e2ad12dc949bb7e851b4ba285e41969b105f
eedo_y <doup2001@naver.com>
2025-08-09 02:27:10 +0900

    📂 file : 프로젝트 디렉토리 수정 (2) - 테스트

--- src/test/java/com/kikihi/store/KikihiApplicationTests.java
+++ src/test/java/com/kikihi/store/KikihiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jiyoung.kikihi;
+package com.kikihi.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-class KikihiBeApplicationTests {
+class KikihiApplicationTests {
 
 	@Test
 	void contextLoads() {
