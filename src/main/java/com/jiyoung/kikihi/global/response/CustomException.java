07fb1e4f67f84d07b31f4c0fd40c9349b1b0b811
eedo_y <doup2001@naver.com>
2025-08-11 14:36:57 +0900

    ✨feat/KIKI-72 : 새로운 응답객체 적용

--- src/main/java/com/jiyoung/kikihi/global/response/CustomException.java
+++ src/main/java/com/jiyoung/kikihi/global/response/CustomException.java
@@ -1,19 +1,23 @@
 package com.jiyoung.kikihi.global.response;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 응답을 하는 커스텀 예외 클래스입니다.
+ * ErrorCode와 필드별 에러 정보를 함께 담아, 일관된 에러 응답을 제공합니다.
+ */
 
 @Getter
-public class CustomException extends RuntimeException {
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+
     private final ErrorCode errorCode;
 
-    public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
+    private final List<FieldErrorResponse> fieldErrorResponses;
 
-    public CustomException(ErrorCode errorCode, String message) {
-        super(message);
-        this.errorCode = errorCode;
-    }
+
 }
 
