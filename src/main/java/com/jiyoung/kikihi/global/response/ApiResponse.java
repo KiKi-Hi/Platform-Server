ba4f236ea6c7f8a4a00865abb2c308848505c1af
eedo_y <doup2001@naver.com>
2025-08-11 16:01:55 +0900

    ♻️ refactor/KIKI-72 : 코드리뷰 반영 - 응답객체에서 NULL 제외하도록 수정

--- src/main/java/com/jiyoung/kikihi/global/response/ApiResponse.java
+++ src/main/java/com/jiyoung/kikihi/global/response/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.jiyoung.kikihi.global.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.micrometer.common.lang.Nullable;
 import org.springframework.http.HttpStatus;
 
@@ -9,7 +10,7 @@ import java.util.List;
 /**
  * API 응답을 표준화하기 위한 레코드 클래스입니다.
  */
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
         @JsonIgnore
         HttpStatus httpStatus,
