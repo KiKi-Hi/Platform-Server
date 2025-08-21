4f3839cf6a8d3c32634f70619e1bcf414eec6988
eedo_y <doup2001@naver.com>
2025-08-21 16:20:58 +0900

    ✨feat/KIKI-48 : DTO 내, 옵션 출력 및 유사 상품 추천 삭제

--- src/main/java/site/kikihi/custom/platform/adapter/in/web/dto/response/product/ProductDetailResponse.java
+++ src/main/java/site/kikihi/custom/platform/adapter/in/web/dto/response/product/ProductDetailResponse.java
@@ -4,7 +4,10 @@ import site.kikihi.custom.platform.domain.product.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * 상품 상세 응답 DTO
@@ -14,12 +17,8 @@ import java.util.List;
  * @param manufacturerName   제조사명
  * @param category           카테고리
  * @param productName        제품명
- * @param discountRate       할인율 (예: 0.15는 15%)
  * @param originalPrice      최저가
- * @param discountedPrice    할인가
  * @param likedByMe          나의 북마크 여부
- * @param deliveryInfo       배송 정보 (배송비, 배송 종류, 예상 도착일 등)
- * @param recommendedItems   어울리는 상품 목록
  * @param cautions           상품 유의사항
  * @param imageUrl           상세 정보 이미지 URL 목록
  */
@@ -45,26 +44,17 @@ public record ProductDetailResponse(
         @Schema(description = "제품명", example = "독거미 Aula F99")
         String productName,
 
-        @Schema(description = "할인율 (예: 0.15=15%)", example = "0.15")
-        double discountRate,
-
         @Schema(description = "정상가(원)", example = "599000.0")
         double originalPrice,
-
-        @Schema(description = "할인가(원)", example = "509000.0")
-        double discountedPrice,
 
         @Schema(description = "북마크(좋아요)한 상품 여부", example = "true")
         boolean likedByMe,
 
-        @Schema(description = "배송 정보 응답 DTO", implementation = DeliveryInfoResponse.class)
-        DeliveryInfoResponse deliveryInfo,
-
-        @Schema(description = "어울리는 추천 상품 목록", implementation = RecommendedItemResponse.class)
-        List<RecommendedItemResponse> recommendedItems,
-
         @Schema(description = "상품 유의사항", example = "도착일은 배송지나 배송사 사정으로 변경 또는 지연될 수 있습니다.")
         String cautions,
+
+        @Schema(description = "상품 옵션")
+        List<ProductOptions> options,
 
         @Schema(description = "상세 정보 이미지 URL 목록", example = "[\"https://example.com/img1.jpg\", \"https://example.com/img2.jpg\"]")
         List<String> imageUrl
@@ -78,12 +68,9 @@ public record ProductDetailResponse(
                 .manufacturerName(product.getManufacturer())
                 .category(product.getCategory())
                 .productName(product.getName())
-                .discountRate(0)
                 .originalPrice(product.getPrice())
-                .discountedPrice(product.getPrice())
                 .likedByMe(false)
-                .deliveryInfo(DeliveryInfoResponse.of(3000, "일반배송", "3일 이내 발송 예정"))
-                .recommendedItems(null)
+                .options(ProductOptions.from(product))
                 .cautions("도착일은 배송지나 배송사 사정으로 변경 또는 지연될 수 있습니다.")
                 .imageUrl(product.getAllDetailImages())
                 .build();
@@ -97,16 +84,67 @@ public record ProductDetailResponse(
                 .manufacturerName(product.getManufacturer())
                 .category(product.getCategory())
                 .productName(product.getName())
-                .discountRate(0)
                 .originalPrice(product.getPrice())
-                .discountedPrice(product.getPrice())
                 .likedByMe(likedByMe)
-                .deliveryInfo(DeliveryInfoResponse.of(3000, "일반배송", "3일 이내 발송 예정"))
-                .recommendedItems(null)
+                .options(ProductOptions.from(product))
                 .cautions("도착일은 배송지나 배송사 사정으로 변경 또는 지연될 수 있습니다.")
                 .imageUrl(product.getAllDetailImages())
                 .build();
     }
+
+
+    /// 내부에서만 사용되는 옵션
+    @Builder
+    private record ProductOptions(
+            String optionName,
+            Double price,
+            String site,
+            String siteUrl
+    ) {
+
+        /// 정적 팩토리 메서드
+        private static ProductOptions of(String optionName, Double price, String site, String siteUrl) {
+            return ProductOptions.builder()
+                    .optionName(optionName)
+                    .price(price)
+                    .site(site)
+                    .siteUrl(siteUrl)
+                    .build();
+        }
+
+        /// 정적 팩토리 메서드
+        public static List<ProductOptions> from(Product product) {
+            List<Map<String, Object>> productOptions = Optional.ofNullable(product.getOptions())
+                    .orElse(Collections.emptyList());
+
+            return productOptions.stream()
+                    /// option_name 없는 건 필터링
+                    .filter(option -> option.get("option_name") != null)
+                    .map(option -> {
+
+                        /// 옵션 명 가져오기
+                        String optionName = (String) option.get("option_name");
+                        Double price = null;
+
+                        /// 가격이 있다면
+                        if (option.get("main_price") != null) {
+                            price = Double.valueOf(option.get("main_price").toString());
+                        }
+
+                        String site = null;
+                        String siteUrl = null;
+                        Object vendorsObj = option.get("vendors");
+                        if (vendorsObj instanceof Map<?, ?> vendors) {
+                            site = (String) vendors.get("shop");
+                            siteUrl = (String) vendors.get("url");
+                        }
+
+                        return of(optionName, price, site, siteUrl);
+                    })
+                    .toList();
+        }
+    }
+
 
 }
 
