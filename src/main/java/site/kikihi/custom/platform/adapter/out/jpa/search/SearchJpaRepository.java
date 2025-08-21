e680d73dbd6b0c8f2d5e19700fcdb6a8607a27c2
eedo_y <doup2001@naver.com>
2025-08-21 17:38:31 +0900

    ✨ feat/KIKI-61 : 검색을 위한 도메인 설정

---
+++ src/main/java/site/kikihi/custom/platform/adapter/out/jpa/search/SearchJpaRepository.java
@@ -0,0 +1,6 @@
+package site.kikihi.custom.platform.adapter.out.jpa.search;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SearchJpaRepository extends JpaRepository<SearchJpaEntity, Long> {
+}
