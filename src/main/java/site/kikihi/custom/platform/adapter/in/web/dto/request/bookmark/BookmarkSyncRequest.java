fb5aa5d4b995ba9d13a32e2e617e50a4d43b1630
eedo_y <doup2001@naver.com>
2025-09-18 17:14:51 +0900

    ✨ feat/71 : 응답 DTO 스웨거 추가

--- src/main/java/site/kikihi/custom/platform/adapter/in/web/dto/request/bookmark/BookmarkSyncRequest.java
+++ src/main/java/site/kikihi/custom/platform/adapter/in/web/dto/request/bookmark/BookmarkSyncRequest.java
@@ -1,14 +1,20 @@
 package site.kikihi.custom.platform.adapter.in.web.dto.request.bookmark;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Schema(
+        name = "[요청][북마크] 북마크 싱크 Request",
+        description = "상품을 하번에 북마크할 때 사용하는 요청 DTO입니다."
+)
 public class BookmarkSyncRequest {
 
     @Schema(description = "북마크할 상품들", example = "6896ed7d5198cf586e933d6e")
-    private List<BookmarkRequest> productIds;
+    private List<@Valid BookmarkRequest> productIds;
+
 
 }
--- src/main/java/site/kikihi/custom/platform/adapter/in/web/dto/response/search/SearchListResponse.java
+++ src/main/java/site/kikihi/custom/platform/adapter/in/web/dto/response/search/SearchListResponse.java
@@ -1,10 +1,15 @@
 package site.kikihi.custom.platform.adapter.in.web.dto.response.search;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import site.kikihi.custom.platform.domain.search.Search;
 import java.util.List;
 
 @Builder
+@Schema(
+        name = "[응답][검색] 최신 검색어 조회 Response",
+        description = "사용자의 검색어 정보를 반환하는 응답 DTO입니다."
+)
 public record SearchListResponse(
         Long searchId,
         String keyword
